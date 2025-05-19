9a2b1e7dcef968aef3cf1e9a06da4451fe0ecc8c
Stainowy <universett0@gmail.com>
2025-05-19 18:04:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sliders = document.querySelectorAll('.slider');
+
+  sliders.forEach((slider) => {
+    const images = slider.querySelectorAll('img');
+    const leftArrow = slider.querySelector('.arrow.left');
+    const rightArrow = slider.querySelector('.arrow.right');
+    let current = 0;
+
+    function showImage(index) {
+      images.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+      });
+    }
+
+    leftArrow.addEventListener('click', () => {
+      current = (current - 1 + images.length) % images.length;
+      showImage(current);
+    });
+
+    rightArrow.addEventListener('click', () => {
+      current = (current + 1) % images.length;
+      showImage(current);
+    });
+
+    showImage(current);
+  });
+});
